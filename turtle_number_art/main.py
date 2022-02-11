cce496a97dcbafb052366b52d52a65c27e89bf66
Lari Liuhamo <Diapolo10@users.noreply.github.com>
2022-02-12 01:02:38 +0200

    [PATCH] Added ability to save output as image file

--- turtle_number_art/main.py
+++ turtle_number_art/main.py
@@ -5,17 +5,35 @@
 Print with Turtle Graphics using numbers
 """
 
-import math
 import turtle
+from datetime import datetime
 from pathlib import Path
 from typing import Generator
 
+from PIL import Image
+
 ROOT_DIR = Path(__file__).parent
 DATA_DIR = ROOT_DIR / 'number_data'
+OUTPUT_DIR = ROOT_DIR / 'output'
 PI_DEC_1K = DATA_DIR / 'pi_dec_1k.txt'
 PI_DEC_1M = DATA_DIR / 'pi_dec_1m.txt'
 
-SCALE = 5
+SCALE = 2
+
+if not OUTPUT_DIR.exists():
+    OUTPUT_DIR.mkdir()
+
+def save_as_png(canvas, filename=None):
+
+    if filename is None:
+        filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    canvas.postscript(file = OUTPUT_DIR / f'{filename}.eps')
+
+    with Image.open(OUTPUT_DIR / f'{filename}.eps') as img:
+        img.save(OUTPUT_DIR / f'{filename}.png', 'png')
+
+    (OUTPUT_DIR / f'{filename}.eps').unlink()
 
 def digits(file: Path) -> Generator[int, None, None]:
     for digit in file.read_text(encoding='utf-8'):
@@ -32,9 +50,11 @@ def main():
     t.speed(0)
 
     for digit in digits(PI_DEC_1K):
-        angle = base_n_angle(digit, 4)
+        angle = base_n_angle(digit, 3.14)
         t.left(angle)
         t.forward(digit * SCALE)
+
+    save_as_png(turtle.getscreen().getcanvas())
     input()
 
 
